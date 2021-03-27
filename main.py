92a284e9bfc2655466a4b4dd3ee8c95e7c29ea8e
vip.egorenko@mail.ru <Tujhtyrj3>
2021-03-27 23:45:02 +0700

    First commit

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from container import container
 
 ifst = open("input.txt").read().split("\n")
 ofst = open("output.txt", "w")
+ofst1 = open("output1.txt", "w")
 
 print("Start.")
 c = container()
 c.InData(ifst)
 ofst.write("Filled container.\n")
 c.OutData(ofst)
-for i in range(len(c.matrices)):
-    print(c.matrices[i])
+c.OutDataFirstType(ofst1)
 c.Clear()
 ofst.write("Empty container.\n")
 c.OutData(ofst)
--- matrices.py
+++ matrices.py
@@ -1,5 +1,6 @@
 class matrices():
     def __init__(self):
+        self.type = ""
         self.size = 0
 
 
