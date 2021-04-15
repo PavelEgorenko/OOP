7037c7ba0970e22ae5ec8c6e64bb292f0de74c94
vipeg <vip.egorenko@mail.ru>
2021-04-15 13:15:39 +0700

    Multimethods

--- multimethods.py
+++ multimethods.py
@@ -1,10 +1,11 @@
 from diagonal_matrix import diagonal_matrix
 from binary_matrix import bin_matrix
+from triangle_matrix import triangle_matrix
 from multimethod import multimethod
 
 
 @multimethod
-def mmm(obj1: diagonal_matrix, obj2: bin_matrix,ofst):
+def mmm(obj1: diagonal_matrix, obj2: bin_matrix, ofst):
     ofst.write("Diagonal and common matrix:\n")
 
 
@@ -20,8 +21,33 @@ def mmm(obj: diagonal_matrix, obj2: diagonal_matrix, ofst):
 
 @multimethod
 def mmm(obj: bin_matrix, obj2: bin_matrix, ofst):
-    print("2")
     ofst.write("Common and common matrix:\n")
+
+
+@multimethod
+def mmm(obj: triangle_matrix, obj2: bin_matrix, ofst):
+    ofst.write("Triangle and common matrix:\n")
+
+
+@multimethod
+def mmm(obj: bin_matrix, obj2: triangle_matrix, ofst):
+    ofst.write("Common and triangle matrix:\n")
+
+
+@multimethod
+def mmm(obj: triangle_matrix, obj2: diagonal_matrix, ofst):
+    ofst.write("triangle and diagonal matrix:\n")
+
+
+@multimethod
+def mmm(obj: diagonal_matrix, obj2: triangle_matrix, ofst):
+    ofst.write("diagonal and triangle matrix:\n")
+
+
+@multimethod
+def mmm(obj: triangle_matrix, obj2: triangle_matrix, ofst):
+    ofst.write("triangle and triangle matrix:\n")
+
 
 if __name__ == "__main__":
     A = diagonal_matrix()
